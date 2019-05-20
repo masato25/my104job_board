d379d1f90df90976354feb088584cc94fd86a4ce
masato25 <leomasato25@gmail.com>
2019-05-20 17:04:11 +0800

    chore: move initiation & add i18n

--- assets/js/application.js
+++ assets/js/application.js
@@ -2,14 +2,32 @@ require("expose-loader?$!expose-loader?jQuery!jquery");
 require("bootstrap/dist/js/bootstrap.bundle.js");
 
 import Vue from "vue";
-// use element ui
-require("./setup/initelement");
+import VueI18n from 'vue-i18n';
+import zhLocale from 'element-ui/lib/locale/lang/zh-TW';
+import enLocale from 'element-ui/lib/locale/lang/en';
+import Element from 'element-ui';
+import 'element-ui/lib/theme-default/index.css';
 
 import VueRouter from 'vue-router'
 import Vuex from 'vuex';
 Vue.config.productionTip = false;
 Vue.use(VueRouter)
 Vue.use(Vuex);
+
+// setup i18n
+Vue.use(VueI18n)
+const messages = {
+  zh: zhLocale,
+  en: enLocale,
+}
+const i18n = new VueI18n({
+  locale: 'zh',
+  messages
+})
+// use element ui
+Vue.use(Element, {
+  i18n: (key, value) => i18n.t(key, value)
+})
 
 import AppRow from "./components/app_row.vue";
 import storep from '~/store/app';
@@ -29,5 +47,6 @@ const router = new VueRouter({
 new Vue({
   el: '#app',
   store,
+  i18n,
   render: h => h(AppRow)
 })
