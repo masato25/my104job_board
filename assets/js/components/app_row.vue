bbbd3787f41b9d08c2cb17884b14229f9b7524df
masato25 <leomasato25@gmail.com>
2019-05-20 17:43:52 +0800

    refactor: reach table views

--- assets/js/components/app_row.vue
+++ assets/js/components/app_row.vue
@@ -12,18 +12,32 @@
                   :data="jobs"
                   v-loading.body="loading"
                   style="width: 100%")
-                el-table-column(prop="updated_at" label="更新時間")
-                el-table-column(prop="job_name" label="職缺名稱")
+                el-table-column(type="expand")
+                  template(slot-scope="props")
+                    p 福利: {{props.row.welfare}}
                 el-table-column(prop="company_name" label="公司名稱")
                   template(slot-scope="scope")
+                    span
+                      | {{scope.row.company_name}}
                     span(v-if="scope.row.link")
                       a(:href="urlwithhttp(scope.row.link)" target="_blank")
-                        | {{ scope.row.company_name }}
-                    span(v-if="!scope.row.link")
-                      | {{ scope.row.company_name }}
+                        i(class="el-icon-attract")
+                el-table-column(prop="job_name" label="職缺名稱")
+                  template(slot-scope="scope")
+                    span
+                      | {{scope.row.job_name}}
+                    span
+                      a(:href="'https://www.104.com.tw/jobbank/custjob/index.php?j=' + scope.row.j + '&c=' + scope.row.c" target="_blank")
+                        i(class="el-icon-thumb")
                 el-table-column(prop="sal_low" label="最低薪")
                 el-table-column(prop="sal_high" label="最高薪")
                 el-table-column(prop="area" label="地區")
+                el-table-column(prop="updated_at" label="更新時間")
+                  template(slot-scope="scope")
+                    div
+                      | {{ getDTime(scope.row.updated_at) }}
+                    div
+                      | [{{ getMtTime(scope.row.updated_at) }}]
           el-row
             el-col(:span=6)
               .grid-conten.bg-purple
@@ -43,6 +57,8 @@
 import AppHeader from './header.vue';
 import NavHead from './navhead.vue';
 import { mapState, mapActions } from 'vuex';
+import moment from 'moment';
+moment.locale('zh');
 
 export default {
   name: 'AppRow',
@@ -71,6 +87,15 @@ export default {
      }
      return uri
    },
+   getDTime(ctime) {
+     const dtime = moment(ctime).format("YYYY-MM-DD");
+     return dtime
+   },
+   getMtTime(ctime) {
+     const mttime = moment(ctime).fromNow();
+     return mttime
+
+   },
    ...mapActions(['getJobs', 'setPageShowLimit', 'setCurrentPage'])
   }
 }
@@ -87,5 +112,8 @@ export default {
   margin-left: 20px;
   margin-right: 20px;
   margin-top: 20px;
+  .el-icon-attract {
+    color: #157a56;
+  }
 }
 </style>
