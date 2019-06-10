348ecd40bef8db8582060047ac09d730064615d7
masato25 <leomasato25@gmail.com>
2019-06-10 15:00:24 +0800

    chore: support updated_at sorting table

--- actions/job_controller.go
+++ actions/job_controller.go
@@ -16,7 +16,7 @@ func JobQuery(c buffalo.Context) error {
 	// Default values are "page=1" and "per_page=20".
 	q := tx.Where("cat = ? and sal_low >= ?", c.Param("cat"), c.Param("salary")).PaginateFromParams(c.Params())
 	// You can order your list here. Just change
-	err := q.All(&jobs)
+	err := q.Order("updated_at desc").All(&jobs)
 	// to:
 	// err := q.Order("created_at desc").All(jobs)
 	if err != nil {
